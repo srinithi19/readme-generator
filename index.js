e412895681f51cbc2edad09f2210834535331894
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-01-21 16:42:22 -0800

    add questions array

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-// TODO: Include packages needed for this application
+const { default: inquirer } = require("inquirer");
 
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const markdown = require("utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    { 
+        type: "input", 
+        name: "title", 
+        message: "What is the name of the project?" 
+    },
+    { 
+        type: "input",
+        name: "description", 
+        message: "Description of the project:" },
+    {
+      type: "input",
+      name: "installation",
+      message: "Describe how to install the project(if applicable):",
+    },
+    { 
+        type: "input", 
+        name: "usage", 
+        message: "What is the project usage for?" 
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Who are the contributors to the project(if applicable)?",
+    },
+    { 
+        type: "input", 
+        name: "test", 
+        message: "Are there any tests included?"
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license for the project:",
+      choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+    },
+    { 
+        type: "input",
+        name: "username", 
+        message: "What is your GitHub username?" 
+    },
+    { 
+        type: "input",
+        name: "email", 
+        message: "What is your email address?" 
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
